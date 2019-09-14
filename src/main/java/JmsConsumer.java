378057800865ec74388f72aabc67d6d8b58a5ed6
LM123Code <1456466514@qq.com>
2019-09-14 17:36:35 +0800

    消费者异步监听

--- src/main/java/JmsConsumer.java
+++ src/main/java/JmsConsumer.java
@@ -1,6 +1,7 @@
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
+import java.io.IOException;
 
 /**
  * @author LM_Code
@@ -8,7 +9,7 @@ import javax.jms.*;
  */
 public class JmsConsumer {
     private static final String ACTIVEMQ_URL = "tcp://101.132.78.78:61616";
-    public static void main(String[] args) throws JMSException {
+    public static void main(String[] args) throws JMSException, IOException {
         //1.创建工厂
         ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
         //2.生成连接并访问
@@ -20,6 +21,7 @@ public class JmsConsumer {
         Queue queue01 = session.createQueue("queue01");
         //创建消息消费者
         MessageConsumer consumer = session.createConsumer(queue01);
+        /**同步阻塞式
         while (true){
             TextMessage receive = (TextMessage) consumer.receive();
             if(receive != null){
@@ -28,6 +30,26 @@ public class JmsConsumer {
                 break;
             }
         }
+        consumer.close();
+        session.close();
+        connection.close();
+         **/
+        /**
+         * 异步监听式
+         */
+        consumer.setMessageListener(new MessageListener() {
+            public void onMessage(Message message) {
+                if(message != null && message instanceof TextMessage){
+                    TextMessage textMessage = (TextMessage)message;
+                    try {
+                        System.out.println("********消费者监听并接收到消息" + textMessage.getText());
+                    } catch (JMSException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        System.in.read();//保持控制台不灭
         consumer.close();
         session.close();
         connection.close();
