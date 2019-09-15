f874196a84e5a7d624fbbc1d92136ff337b9b708
LM123Code <1456466514@qq.com>
2019-09-15 09:42:26 +0800

    activeMQ学习之topic模式练习

---
+++ src/main/java/JmsConsumer_topic.java
@@ -0,0 +1,57 @@
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+import java.io.IOException;
+
+/**
+ * @author LM_Code
+ * @create 2019-09-15-8:55
+ */
+public class JmsConsumer_topic {
+    private static final String ACTIVEMQ_URL = "tcp://101.132.78.78:61616";
+
+    public static void main(String[] args) throws JMSException, IOException {
+        //1.创建工厂
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
+        //2.生成连接并访问
+        Connection connection = activeMQConnectionFactory.createConnection();
+        connection.start();
+        //3.创建会话
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        //4.创建目的地
+        Topic topic = session.createTopic("topic-atguigu");
+        //创建消息消费者
+        MessageConsumer consumer = session.createConsumer(topic);
+        //同步阻塞式
+         while (true){
+            TextMessage receive = (TextMessage) consumer.receive();
+            if(receive != null){
+                System.out.println("*****消费者接受到消息" + receive.getText());
+            }else {
+                break;
+            }
+         }
+         consumer.close();
+         session.close();
+         connection.close();
+        /**
+         * 异步监听式
+         */
+        /*consumer.setMessageListener(new MessageListener() {
+            public void onMessage(Message message) {
+                if (message != null && message instanceof TextMessage) {
+                    TextMessage textMessage = (TextMessage) message;
+                    try {
+                        System.out.println("********消费者监听并接收到消息" + textMessage.getText());
+                    } catch (JMSException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        System.in.read();//保持控制台不灭
+        consumer.close();
+        session.close();
+        connection.close();*/
+    }
+}
---
+++ src/main/java/JmsProduce_topic.java
@@ -0,0 +1,37 @@
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+/**
+ * @author LM_Code
+ * @create 2019-09-15-8:52
+ */
+public class JmsProduce_topic {
+    private static final String ACTIVEMQ_URL = "tcp://101.132.78.78:61616";
+    public static void main(String[] args) throws JMSException {
+        //1创建工厂
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
+        //2通过工厂获取连接connection,并启动访问
+        Connection connection = activeMQConnectionFactory.createConnection();
+        connection.start();
+        //3创建会话session,两个参数事务与签收
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        //4.创建目的地，队列还是主题
+        Topic topic = session.createTopic("topic-atguigu");
+        //5.创建消息的生产者，确定往哪个主题里发送
+        MessageProducer producer = session.createProducer(topic);
+        //6.通过生产者发送消息到主题
+        for (int i = 0; i < 3; i++) {
+            //7.创建消息
+            TextMessage textMessage = session.createTextMessage("msg----" + i);
+            //8.通过消息生产者发送给mq
+            producer.send(textMessage);
+        }
+        //释放资源
+        producer.close();
+        session.close();
+        connection.close();
+
+        System.out.println("*********消息发布到MQ*********");
+    }
+}
